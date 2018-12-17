bc8b79b9eabfe9aa013e2a387da1491ac77c256c
dimasrio33 <dimas.0158@students.amikom.ac.id>
2018-12-17 21:46:20 +0700

    First commit

--- app/src/main/java/com/example/user/carisupir/HomeFragment.java
+++ app/src/main/java/com/example/user/carisupir/HomeFragment.java
@@ -6,13 +6,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    Button btn_order;
+    View v;
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+       v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        btn_order = (Button) v.findViewById(R.id.btn_order);
+        btn_order.setOnClickListener(this);
+
+
+       return v;
+
+
+
     }
 
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
 }
